4ea33c1ae087e1099ad7c85f79b949044291f81e
Cameron Cross <cameroncros@gmail.com>
2014-04-14 16:10:53 +1000

    Got the code working and added some slightly better error handling

--- src/com/cameroncros/partone/UDPClient.java
+++ src/com/cameroncros/partone/UDPClient.java
@@ -1,27 +1,29 @@
 package com.cameroncros.partone;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class UDPClient {
-	public static void main(String args[]) throws Exception    
-	{      
-		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
+	public static void main(String args[])    
+	{  
+		try {
 		DatagramSocket clientSocket = new DatagramSocket();  
 		InetAddress IPAddress = InetAddress.getByName("localhost");   
 		byte[] sendData = new byte[1024];       
 		byte[] receiveData = new byte[1024];  
-		String sentence = inFromUser.readLine();    
+		String sentence = "Hello Server, who are you? I am 7193432";    
 		sendData = sentence.getBytes(); 
-		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876); 
+		DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 4010); 
 		clientSocket.send(sendPacket);  
 		DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); 
 		clientSocket.receive(receivePacket);   
 		String modifiedSentence = new String(receivePacket.getData());  
 		System.out.println("FROM SERVER:" + modifiedSentence);    
 		clientSocket.close();  
+		}
+		catch (Exception e) {
+			System.out.println("Failed to run: "+ e.getLocalizedMessage());
+		}
 	} //See more at: http://systembash.com/content/a-simple-java-udp-server-and-udp-client/#sthash.CAm9v4QA.dpuf
 }
--- src/com/cameroncros/partone/UDPServer.java
+++ src/com/cameroncros/partone/UDPServer.java
@@ -13,36 +13,48 @@ import java.net.InetAddress;
  */
 
 public class UDPServer {
-	public static void main(String args[]) throws Exception       
+	public static void main(String args[])     
 	{
+		DatagramSocket serverSocket;
 		/**
 		 * create new socket on port 9876
 		 */
-		DatagramSocket serverSocket = new DatagramSocket(9876);   
+		try {
+			serverSocket = new DatagramSocket(4010);
+		} 
+		catch (Exception e) {
+			System.out.println("Failed to open a socket: " + e.getLocalizedMessage());
+			return;
+		}
 		/**
 		 * buffers for receiveing and sending data
 		 */
 		byte[] receiveData = new byte[1024];          
-		byte[] sendData = new byte[1024];         
-		while(true)               
-		{           
-			/**
-			 * This variable is a class that contains the packet we are about to receive
-			 */
-			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);  
-			/**
-			 * 
-			 */
-			serverSocket.receive(receivePacket);     
-			String sentence = new String( receivePacket.getData());     
-			System.out.println("RECEIVED: " + sentence);            
-			InetAddress IPAddress = receivePacket.getAddress();          
-			int port = receivePacket.getPort();             
-			String capitalizedSentence = sentence.toUpperCase();         
-			sendData = capitalizedSentence.getBytes();            
-			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);      
-			serverSocket.send(sendPacket);            
-			}     
-		} //See more at: http://systembash.com/content/a-simple-java-udp-server-and-udp-client/#sthash.CAm9v4QA.dpuf
+		byte[] sendData = new byte[1024];
+		try {
+			while(true)               
+			{           
+				/**
+				 * This variable is a class that contains the packet we are about to receive
+				 */
+				DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);  
+				/**
+				 * 
+				 */
+				serverSocket.receive(receivePacket);     
+				String sentence = new String( receivePacket.getData());     
+				System.out.println("RECEIVED: " + sentence);            
+				InetAddress IPAddress = receivePacket.getAddress();          
+				int port = receivePacket.getPort();             
+				sentence = "Hello, my name is Cameron's Plagerised UDP Server And my Id is 7193432";         
+				sendData = sentence.getBytes();            
+				DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);      
+				serverSocket.send(sendPacket);       
+			}
+		}
+		catch (Exception e) {
+			serverSocket.close();
+		}
+	} //See more at: http://systembash.com/content/a-simple-java-udp-server-and-udp-client/#sthash.CAm9v4QA.dpuf
 
 }
